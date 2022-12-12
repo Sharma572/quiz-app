cf148286f845837cfc266f44679c3957bcee0537
Raunak Sharma <raunakk572@gmail.com>
2022-12-12 16:35:38 +0530

    prevent reload and change browser tab alert done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Quiz from "./component/Quiz";
 import { Routes, Route } from "react-router-dom";
 import ShowAnswer from "./component/ShowAnswer";
@@ -10,16 +10,52 @@ import Timer from "@amplication/react-compound-timer/build";
 const App = () => {
 
   const [quizState, dispatch] = useContext(QuizContest);
-  
 
-  // document.getElementById(minutes).innerText === 1200000 ? "text-red-300" : "text-yellow-900"
+  
+  const [tabHasFocus, setTabHasFocus] = useState(true);
+
+  // Code for prevent to detect tab change. 
+  useEffect(() => {
+    const handleFocus = () => {
+      console.log("Tab has focus");
+      setTabHasFocus(true);
+    };
+
+    const handleBlur = () => {
+      alert("Warning! You can't change tab");
+      setTabHasFocus(false);
+    };
+
+    window.addEventListener("focus", handleFocus);
+    window.addEventListener("blur", handleBlur);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+      window.removeEventListener("blur", handleBlur);
+    };
+  }, []);
+
+
+
+  // Refresh is disabled here
+  useEffect(() => {
+    window.addEventListener("beforeunload", alertUser);
+    return () => {
+      window.removeEventListener("beforeunload", alertUser);
+    };
+  }, []);
+  const alertUser = (e) => {
+    e.preventDefault();
+    e.returnValue = "❌ Refresh Page is not allowed";
+  };
+  
   return (
     <>
       {quizState.timer && (
         <Timer initialTime={1200000} direction="backward">
           {() => (
             <span className="text-xl text-center text-green-900 absolute top-2 left-36 bg-yellow-200 p-3">
-              <Timer.Minutes /> minutes <Timer.Seconds /> seconds
+             <span className="text-red-500 "><Timer.Minutes /></span>  minutes <Timer.Seconds /> seconds
             </span>
           )}
         </Timer>
