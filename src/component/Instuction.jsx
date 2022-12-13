a0f3c6bad5d10b3f75fc3d615bff0c74c198584a
Raunak Sharma <raunakk572@gmail.com>
2022-12-13 14:00:28 +0530

    full screen mode enable

--- src/component/Instuction.jsx
+++ src/component/Instuction.jsx
@@ -13,6 +13,16 @@ const Instuction = () => {
       navigate(path);
     // console.log("Timer", quizState);
     
+    const toggleFullSceen = () => {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+      } else {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        }
+      }
+    };
+
   return (
     <>
       <div className="instruction-container text-red-700 container">
@@ -41,7 +51,7 @@ const Instuction = () => {
         </ul>
         <button
           className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          onClick={routeChange}
+          onClick={ () => {toggleFullSceen() ; routeChange() } }
         >
           Start
         </button>
